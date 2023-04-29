915d41d4f765b122235f6b7b4a2ceaeb39fb7969
Leo Saban <leodsaban@gmail.com>
2023-04-29 11:36:05 -0600

    workshop1 completed

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,39 +1,18 @@
-// import { FlatList } from "react-native";
-// import { Avatar, ListItem } from 'react-native-elements';
-
-// const DirectoryScreen = (props) => {
-//     const renderDirectoryItem = ({item: campsite }) => {
-//         return (
-//             <ListItem onPress={() => props.onPress(campsite.id)}>
-//                 <Avatar source={campsite.image} rounded />
-//                 <ListItem.Content>
-//                     <ListItem.Title>{campsite.name}</ListItem.Title>
-//                     <ListItem.Subtitle>
-//                         {campsite.description}
-//                     </ListItem.Subtitle>
-//                 </ListItem.Content>
-//             </ListItem>
-//         )
-//     }
-
-//     return (
-//         <FlatList 
-//             data={props.campsites}
-//             renderItem={renderDirectoryItem}
-//             keyExtractor={(item) => item.id.toString()}
-//         />
-//     )
-// }
-
-// export default DirectoryScreen;
-
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-const DirectoryScreen = (props) => {
+const DirectoryScreen = ({ navigation }) => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
-            <ListItem onPress={() => props.onPress(campsite.id)}>
+            <ListItem
+                onPress={() =>
+                    navigation.navigate('CampsiteInfo', { campsite })
+                }
+            >
                 <Avatar source={campsite.image} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{campsite.name}</ListItem.Title>
@@ -46,7 +25,7 @@ const DirectoryScreen = (props) => {
     };
     return (
         <FlatList
-            data={props.campsites}
+            data={campsites}
             renderItem={renderDirectoryItem}
             keyExtractor={(item) => item.id.toString()}
         />
