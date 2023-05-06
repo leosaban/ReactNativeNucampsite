b15f0eb8b3c68d2fc5fc44b6d57d1192496c1084
Leo Saban <leodsaban@gmail.com>
2023-05-06 10:24:50 -0600

    workshop 2

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,6 +1,16 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../shared/baseUrl';
 
+
+export const postComment = createAsyncThunk('comments/postComment', async (payload, { dispatch, getState }) => {
+    setTimeout(() => {
+        const { comments } = getState();
+        payload.date = new Date().toISOString();
+        payload.id = comments.commentsArray.length;
+        dispatch(addComment(payload));
+    }, 2000);
+})
+
 export const fetchComments = createAsyncThunk(
     'comments/fetchComments',
     async () => {
@@ -18,7 +28,9 @@ export const fetchComments = createAsyncThunk(
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: { addComment:(state, action) => {
+        state.commentsArray.push(action.payload)
+    }},
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -39,3 +51,4 @@ const commentsSlice = createSlice({
 });
 
 export const commentsReducer = commentsSlice.reducer;
+export const {addComment} = commentsSlice.actions;
