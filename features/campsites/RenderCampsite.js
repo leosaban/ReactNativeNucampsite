915c13b311a27d337ee8b8cb80ef54ce395d3a26
Leo Saban <leodsaban@gmail.com>
2023-05-08 12:36:09 -0600

    animations and animatable

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,45 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
+import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
     if (campsite) {
         return (
-            <Card containerStyle={styles.cardContainer}>
-                <Card.Image source={{ uri: baseUrl + campsite.image }}>
-                    <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text
-                            style={styles.cardText}
-                        >
-                            {campsite.name}
-                        </Text>
-                    </View>
-                </Card.Image>
-                <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={props.isFavorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        raised
-                        reverse
-                        onPress={() =>
-                            props.isFavorite
-                                ? console.log('Already set as a favorite')
-                                : props.markFavorite()
-                        }
-                    />
-                    <Icon
-                            name={'pencil'}
+            <Animatable.View
+                animation='fadeInDownBig'
+                duration={2000}
+                delay={1000}
+            >
+                <Card containerStyle={styles.cardContainer}>
+                    <Card.Image source={{ uri: baseUrl + campsite.image }}>
+                        <View style={{ justifyContent: 'center', flex: 1 }}>
+                            <Text style={styles.cardText}>{campsite.name}</Text>
+                        </View>
+                    </Card.Image>
+                    <Text style={{ margin: 20 }}>{campsite.description}</Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={props.isFavorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            raised
+                            reverse
+                            onPress={() =>
+                                props.isFavorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                            }
+                        />
+                        <Icon
+                            name='pencil'
                             type='font-awesome'
                             color='#5637DD'
                             raised
                             reverse
-                            onPress={() => props.onShowModal()}
-                    />
-                </View>
-            </Card>
+                            onPress={props.onShowModal}
+                        />
+                    </View>
+                </Card>
+            </Animatable.View>
         );
     }
     return <View />;
@@ -50,19 +53,19 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
-    }, 
+    },
     cardRow: {
-        alignItems:'center',
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
         flex: 1,
-        flexDirection:'row',
+        flexDirection: 'row',
         margin: 20
     },
     cardText: {
-        textShadowColor: 'rgba(0,0,0,1)',
-        textShadowOffset: {width: -1, height: 1},
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowOffset: { width: -1, height: 1 },
         textShadowRadius: 20,
-        textAlign:'center',
+        textAlign: 'center',
         color: 'white',
         fontSize: 20
     }
